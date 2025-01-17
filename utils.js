b89c76ade65795e61865dadf659fd740339a0a70
robbrotherton <robert.brotherton@gmail.com>
2025-01-17 12:01:04 -0500

    addresses vh issue on mobile

---
+++ utils.js
@@ -0,0 +1,11 @@
+function updateVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', updateVh);
+window.addEventListener('orientationchange', updateVh);
+
+// Initial call to set the value
+console.log("setting vh");
+updateVh();
